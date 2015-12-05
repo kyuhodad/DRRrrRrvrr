1872ac6d76772c5970d27f3dc4c6b279a60cc23d
kyuhodad <kyuhodad@gmail.com>
2015-12-04 23:43:04 -0500

    Final touch for submit

--- src/js/services/oauthConfig.js
+++ src/js/services/oauthConfig.js
@@ -1,9 +1,10 @@
 // -----------------------------------------------------------------------------
 // "OAuthConfig" value: Holding the CLIENT_ID & SCOPE to get authorized for
 // Google Service.
+// NOTE: The CLIENT_ID, below, is not valid one. Please, use your own valid id to test.
 // -----------------------------------------------------------------------------
 angular.module('DRRrrRrvrr')
 .value('OAuthConfig', {
-  CLIENT_ID: '905693818138-c8q6jtqnq2isv36o0aeqbs92lk66d0m5.apps.googleusercontent.com',
+  CLIENT_ID: '818138905693-v36o0aeqbsc8q6jtqnq2is92lk66d0m5.apps.googleusercontent.com',
   SCOPES: ['https://www.googleapis.com/auth/drive.readonly']
 });
